e335cc767d0df0cd9a4fd88605b14b53cbddf78a
akjalda <akshathjalda@gmail.com>
2021-04-15 17:49:07 +0530

    ... + Undo

--- src/posts.js
+++ src/posts.js
@@ -13,7 +13,9 @@ import {
     TextInput,
 } from 'react-admin';
 
-
+const PostTitle = ({ record }) => {
+    return <span>Post { record ? `"${ record.title }` : '' }</span>;
+};
 
 export const PostList = props => (
     <List {...props}>
@@ -29,7 +31,7 @@ export const PostList = props => (
 );
 
 export const PostEdit = props => (
-    <Edit {...props}>
+    <Edit title = { <PostTitle /> } {...props}>
         <SimpleForm>
             <TextField disabled source = "id" />
             <ReferenceInput source = "userId" reference="users">
